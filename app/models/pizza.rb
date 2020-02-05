fa1c8c3698b17bb73b410271935b1beec12a5ad8
Dino <dinokunovac@gmail.com>
2020-02-05 11:26:45 -0500

    until next time

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,7 @@
 class Pizza < ApplicationRecord
+    has_many :restaurantpizzas
+    has_many :restaurants, through: :restaurantpizzas
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :ingredients, presence: true
 end
